4b185a4ea6f9be2cca5f88d38f9187a604f23189
Herman Streltsov <mail@streltsov.dev>
2021-04-20 12:21:36 +0300

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,41 +1,51 @@
 import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Layout.Spacing
+import XMonad.Layout.Monitor
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.LayoutModifier
+
+pip = monitor
+      {
+        prop = Role "PictureInPicture"
+      , rect = Rectangle 1440 810 480 270 --(1280-150) (800-150) 150 150
+      , persistent = True
+      , opacity = 0.2
+      }
 
 myManageHook = composeAll
-  [ stringProperty "WM_WINDOW_ROLE" =? "PictureInPicture" --> doIgnore
-  , className =? "TelegramDesktop" --> doShift "7" ]
+  [ className =? "TelegramDesktop" --> doShift "7" ]
 
 main = do
-  xmonad $ ewmh $ docks $ defaultConfig {
+      xmonad $ ewmh $ docks $ defaultConfig {
           modMask               = mod4Mask
         , terminal              = "termite"
         , focusedBorderColor    = "#665c54"
         , normalBorderColor     = "#000000"
         , borderWidth           = 1
-        , manageHook            = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        , layoutHook            = smartSpacing 2 $ smartBorders $ noBorders Full ||| avoidStruts(ResizableTall 1 (1/100) (1/2) [])
-         } `additionalKeys` [
+        , manageHook            = manageDocks <+> myManageHook <+> manageHook defaultConfig <+> manageMonitor pip
+        , layoutHook            = ModifiedLayout pip $ smartSpacing 2 $ smartBorders $ noBorders Full ||| avoidStruts(ResizableTall 1 (1/100) (1/2) [])
+                                            } `additionalKeys` [
 
         -- Apps and tools
           ((mod4Mask, xK_f),                   spawn "firefox")
-        , ((mod4Mask, xK_n),                   spawn "~/.firefox-nightly/firefox")
-        , ((mod4Mask, xK_d),                   spawn "goldendict")
-        , ((mod4Mask, xK_v),                   spawn "pavucontrol")
+          , ((mod4Mask, xK_n),                   spawn "~/.firefox-nightly/firefox")
+          , ((mod4Mask, xK_d),                   spawn "goldendict")
+          , ((mod4Mask, xK_v),                   spawn "pavucontrol")
 
+          , ((mod4Mask, xK_u), broadcastMessage ToggleMonitor >> refresh)
         -- Special commands
-        , ((mod4Mask, xK_End),                 spawn "poweroff")
-        , ((mod4Mask .|. shiftMask, xK_End),   spawn "reboot")
-        , ((0, xK_Print),                      spawn "maim -u | tee ~/pics/screenshots/$(date +%s).png | xclip -selection clipboard -t image/png")
-        , ((mod4Mask, xK_m),                   spawn "maim -s -u | xclip -selection clipboard -t image/png -i")
-        , ((mod4Mask, xK_x),                   spawn "slock")
+          , ((mod4Mask, xK_End),                 spawn "poweroff")
+          , ((mod4Mask .|. shiftMask, xK_End),   spawn "reboot")
+          , ((0, xK_Print),                      spawn "maim -u | tee ~/pics/screenshots/$(date +%s).png | xclip -selection clipboard -t image/png")
+          , ((mod4Mask, xK_m),                   spawn "maim -s -u | xclip -selection clipboard -t image/png -i")
+          , ((mod4Mask, xK_x),                   spawn "slock")
 
         -- Layout bindings
-        , ((mod4Mask, xK_b), sendMessage ToggleStruts)
-        , ((mod4Mask, xK_i), sendMessage MirrorShrink)
-        , ((mod4Mask, xK_o), sendMessage MirrorExpand)
-        ]
+          , ((mod4Mask, xK_b), sendMessage ToggleStruts)
+          , ((mod4Mask, xK_i), sendMessage MirrorShrink)
+          , ((mod4Mask, xK_o), sendMessage MirrorExpand)
+                                                               ]
